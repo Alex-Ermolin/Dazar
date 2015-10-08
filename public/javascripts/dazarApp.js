8b1b666b84ebe304e9e80a9895d008a0f89a55c6
Alex-Ermolin <greenclick098@gmail.com>
2015-10-08 23:36:34 +0300

    added further changes to the login, register routes

--- public/javascripts/dazarApp.js
+++ public/javascripts/dazarApp.js
@@ -1,11 +1,11 @@
-var dazarApp = angular.module('dazarApp', ['ngRoute']);
+var dazarApp = angular.module('dazarApp', ['ngRoute', 'ngResource']);
 
 dazarApp.run(function($rootScope) {
-    $rootScope.authenticated = false;
+    $rootScope.isAuthenticated = false;
     $rootScope.current_user = '';
     $rootScope.signout = function() {
         $http.get('/auth/signout');
-        $rootScope.authenticated = false;
+        $rootScope.isAuthenticated = false;
         $rootScope.current_user = '';
     };
 });
@@ -19,6 +19,11 @@ dazarApp.config(function($routeProvider) {
             controller: 'mainController'
         })
 
+        .when('/#', {
+            templateUrl: 'main.html',
+            controller: 'mainController'
+        })
+
         //login page
         .when('/login', {
             templateUrl: 'login.html',
@@ -26,38 +31,55 @@ dazarApp.config(function($routeProvider) {
         })
 
         //registration page
-        .when('/register', {
+        .when('/signup', {
             templateUrl: 'register.html',
             controller: 'authController'
         })
 });
 
-dazarApp.controller('mainController', function($scope){
-    $scope.posts = [];
-    $scope.newPost = {created_by: '', text: '', created_at: ''};
+dazarApp.factory('postService', function($resource) {
+    return $resource('/api/posts/:id');
+});
+
+dazarApp.controller('mainController', function(postService, $scope, $rootScope){
+    $scope.posts = postService.query();
+    $scope.newPost = {created_by: '', text: '', created_at: '', edits: []};
 
     $scope.post = function(){
-        $scope.newPost.created_at = Date.now();
-        $scope.posts.push($scope.newPost);
-        $scope.newPost = {created_by: '', text: '', created_at: ''};
+        $scope.newPost.created_by = $rootScope.current_user;
+        //$scope.newPost.created_at = Date.now();
+        postService.save($scope.newPost, function() {
+            $scope.posts = postService.query();
+            $scope.newPost = {created_by: '', text: '', created_at: '', edits: []};
+        });
     };
 });
 
-dazarApp.controller('authController', function($scope) {
+dazarApp.controller('authController', function($scope, $location, $http, $rootScope) {
     $scope.user = {username: '', password: ''};
     $scope.error_message = '';
 
-    // postService.getAll().success(function(data){
-    //    	$scope.posts = data;
-    //  	});
-
     $scope.login = function(){
-        //placeholder until authentication is implemented
-        $scope.error_message = 'login request for ' + $scope.user.username;
+        $http.post('/auth/login', $scope.user).success(function(data) {
+            if(data.state == 'success') {
+                $rootScope.isAuthenticated = true;
+                $rootScope.current_user = data.username;
+                $location.path('/');
+            } else {
+                $scope.error_message = data.error;
+            }
+        });
     };
 
     $scope.register = function(){
-        //placeholder until authentication is implemented
-        $scope.error_message = 'registeration request for ' + $scope.user.username;
+        $http.post('/auth/signup', $scope.user).success(function(data) {
+            if(data.state == 'success') {
+                $rootScope.isAuthenticated = true;
+                $rootScope.current_user = data.username;
+                $location.path('/');
+            } else {
+                $scope.error_message = data.error;
+            }
+        })
     };
 });
